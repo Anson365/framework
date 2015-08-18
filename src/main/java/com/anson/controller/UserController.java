9b017ad39e26a6543c85dc713f3d1a2f0a7bccf4
Anson2015 <297856005@qq.com>
2015-08-18 17:44:08 +0800

    增加MD5加密及拼音解析

--- src/main/java/com/anson/controller/UserController.java
+++ src/main/java/com/anson/controller/UserController.java
@@ -3,25 +3,39 @@ package com.anson.controller;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 import com.anson.model.Users;
 import com.anson.service.interfaces.IUserService;
+import com.anson.util.MD5Parse;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
-//	@RequestMapping(value="/userInfo",method=RequestMethod.GET)
-//	public String getUserInfo(){
-//		System.out.println("hello");
-//		return "index";
-//	}
+	
+	@Autowired
 	private IUserService userService;
+	
+	@RequestMapping("/signUp")
+	@ResponseBody
+	public Users signUp(HttpServletRequest request){
+		String phoneNumber = request.getParameter("phoneNumber");
+		String password = request.getParameter("password");
+		String userName = request.getParameter("userName");
+		if(phoneNumber.matches("d{13}")){
+			//TODO
+		}
+		Users user = new Users(0L,userName,MD5Parse.parseStr2md5(password), phoneNumber,new Date());
+		return user;
+	}
 	
 	@RequestMapping(value="/{id}/user",method=RequestMethod.GET)
 	public String getUserInfo(@PathVariable long id){
@@ -39,19 +53,8 @@ public class UserController {
 		
 		Users user = new Users(1L,"hahaha","12345678901","asdf",new Date());
 		int user1 = userService.updateUserById(user);
-//		System.out.println(JSON.toJSONString(user));
-//		Users user1 = userService.getUserById(id);
 		System.out.println(JSON.toJSONString(user1));
 		return "index";
 	}
-	
-	public IUserService getUserService() {
-		return userService;
-	}
-	@Autowired
-	public void setUserService(IUserService userService) {
-		this.userService = userService;
-	}
-	
 	
 }
