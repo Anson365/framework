cafd5872ca44805fa7f18da1a0329461c1d5dd9f
Anson2015 <297856005@qq.com>
2015-08-21 16:20:34 +0800

    增强redis功能，拓展对对象的存储

--- src/main/java/com/anson/nosql/RedisUtil.java
+++ src/main/java/com/anson/nosql/RedisUtil.java
@@ -3,6 +3,7 @@ package com.anson.nosql;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
+import org.mybatis.caches.redis.SerializeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +17,7 @@ public class RedisUtil {
 	private JedisPool jedisPool;
 	
 	
-	private  final int tokenExpire = 60* 60*24*15;
+	private  final int tokenExpire = 60* 60*24*15; //设置时间为15天
 	private static final Logger logger = Logger.getLogger(RedisUtil.class);
 	
 	
@@ -34,17 +35,18 @@ public class RedisUtil {
 	}
 	
 	/**
-	 * 设置存取内容以及时间
-	 * @param key String
-	 * @param value String
+	 * 设置存取内容以及时间(利用序列化可存储所以的对象)
+	 * @param key 
+	 * @param value 
 	 * @param expire int 有效时间，0默认为永久（seconds）
 	 */
-	public void set(String key,String value,int expire){
+	public void set(Object key,Object value,int expire){
 		Jedis jedis = jedisPool.getResource();
-		jedis.set(key, value);
+		byte[] keyB = SerializeUtil.serialize(key);
+		jedis.set(keyB, SerializeUtil.serialize(value));
 		if(expire>0){
 			logger.debug(key+":"+expire+"="+value);
-			jedis.expire(key, expire);
+			jedis.expire(keyB, expire);
 		}
 	}
 	
@@ -52,9 +54,10 @@ public class RedisUtil {
 	 * @param key
 	 * @return
 	 */
-	public String get(String key){
+	public Object get(Object key){
 		Jedis jedis = jedisPool.getResource();
-		String result = jedis.get(key);
+		byte[] resultTemp = jedis.get(SerializeUtil.serialize(key));
+		Object result = SerializeUtil.unserialize(resultTemp);  //反序列化操作
 		logger.debug(key+":"+"="+result);
 		return result ;
 	}
@@ -65,23 +68,26 @@ public class RedisUtil {
 	 * @param expire int 设置新的缓存时间
 	 * @return
 	 */
-	public String getWithNewExpire(String key,int expire){
+	public Object getWithNewExpire(Object key,int expire){
 		Jedis jedis = jedisPool.getResource();
-		String result = jedis.get(key);
+		byte[] keyB = SerializeUtil.serialize(key);
+		byte[] resultTemp = jedis.get(keyB);
+		Object result = SerializeUtil.unserialize(resultTemp);
 		if(result != null){
 			if(expire >0){
-				jedis.expire(key,expire);
+				jedis.expire(keyB,expire);
 			}else{
-				jedis.del(key);
+				jedis.del(keyB);
 			}
 		}
 		logger.debug(key+":"+expire+"="+result);
 		return result;
 	}
 	
-	public void remove(String key){
+	public void remove(Object key){
 		Jedis jedis = jedisPool.getResource();
 		logger.debug(key+"=>remove");
-		jedis.del(key);
+		byte[] keyB = SerializeUtil.serialize(key);
+		jedis.del(keyB);
 	}
 }
--- src/test/java/TestUserService.java
+++ src/test/java/TestUserService.java
@@ -5,6 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.alibaba.fastjson.JSON;
+import com.anson.nosql.RedisUtil;
 import com.anson.service.interfaces.IUserService;
 import com.anson.user.model.Users;
 
@@ -22,13 +23,19 @@ public class TestUserService {
 		Users user = userService.getUserById(1);
 		System.out.println(JSON.toJSONString(user));
 	}
-	
-//	public void testRedis(){
-//		Jedis jredis = new Jedis("sdf",23434);
-//		jredis.set("safd", "sdf");
-//		jredis.expire("sdfd",39089809);
-//		jredis.get("sdf");
-//		
-//	}
+
+	@Autowired
+	private RedisUtil redisUtil;
+
+	@Test
+	public void test() {
+		long key = 20,value = 2;
+		String token = redisUtil.genToken(20);
+		redisUtil.set(key, value, 100);
+		long id = Long.valueOf(redisUtil.get(token).toString());
+		long id2 = Long.parseLong(redisUtil.get(id).toString());
+		System.out.println(id);
+		System.out.println(id2);
+	}
 
 }
